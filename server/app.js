5db0c6a73acae2093554482e1c6f2990febe3487
Parth <parthsali04@gmail.com>
2022-12-10 22:35:37 +0530

    all files are removed

--- server/app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require("express");
-const app = express();
-
-const port = 3000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
